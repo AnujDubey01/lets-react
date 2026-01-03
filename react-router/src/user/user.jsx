aee9cb61508efb4378f456e20802f05216081403
Anuj Dubey <anujdubey.ok@gmail.com>
2026-01-03 11:43:02 +0530

    Fix User component function name capitalization

--- react-router/src/user/user.jsx
+++ react-router/src/user/user.jsx
@@ -1,16 +1,13 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 
-function User() {
+function user( ) {
     const { userid } = useParams()
     return (
-        <div className="py-16 bg-white">
-            <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
-                <h1 className="text-4xl font-bold text-gray-900">User Profile</h1>
-                <p className="mt-4 text-xl">User ID: {userid}</p>
-            </div>
-        </div>
+      
+             <div className='bg-gray-600 text-white text-3xl p-4'>User: {userid}</div>
+       
     )
 }
 
-export default User
+export default user

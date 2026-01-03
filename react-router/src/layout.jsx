e048e8e4efa1ca9c22c38512510a397e435f401c
Anuj Dubey <anujdubey.ok@gmail.com>
2026-01-03 11:35:34 +0530

    Add React Router with Github API integration and user routes

--- react-router/src/layout.jsx
+++ react-router/src/layout.jsx
@@ -1,20 +1,17 @@
 import { useState } from 'react'
 import './App.css'
-import Home from './home/home' // Change made by Amazon Q - Fixed import path
-import Header from './header/header' // Change made by Amazon Q - Fixed import path
-import Footer from './footer/footer' // Change made by Amazon Q - Fixed import path
+import Header from './header/header'
+import Footer from './footer/footer'
 import {Outlet} from 'react-router-dom'
 
-function Layout() { // Change made by Amazon Q - Fixed component name capitalization
-
+function Layout() {
   return (
     <>
-      <Header /> {/* Change made by Amazon Q - Fixed component capitalization */}
+      <Header />
       <Outlet />
-      <Footer /> {/* Change made by Amazon Q - Fixed component capitalization */}
-
+      <Footer />
     </>
   )
 }
 
-export default Layout // Change made by Amazon Q - Fixed export name
+export default Layout
--- react-router/src/main.jsx
+++ react-router/src/main.jsx
@@ -2,18 +2,26 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 
 // Change made by Amazon Q - Fixed import paths to match actual file locations
 import Layout from './layout.jsx'
 import Home from './home/home.jsx'
 import About from './about/about.jsx'
 import Contact from './contact/contact.jsx'
+import User from './user/user.jsx'
+import Github from './github/github.jsx'
 
+
+
+// there are two ways to define routes in react-router-dom v6.4+
+// 1. Using createBrowserRouter (commented out below)
+// 2. Using JSX inside App.jsx (currently being used)
+/*
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Layout />, // Change made by Amazon Q - Fixed component capitalization
+    element: <Layout />, 
     children: [
       {
         path: "",
@@ -30,6 +38,20 @@ const router = createBrowserRouter([
     ]
   },
 ]);
+*/
+// Using createRoutesFromElements
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="user/:userid" element={<User />} />
+      <Route path="github" element={<Github />} />
+    </Route>
+  )
+)
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>

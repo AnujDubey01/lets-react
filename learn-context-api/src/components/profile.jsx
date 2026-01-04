ab776e9c507cbbac8403c1f95d60bbb479e1990f
AnujDubey01 <anujdubey.ok@gmail.com>
2026-01-04 14:17:46 +0530

    changes ui

--- learn-context-api/src/components/profile.jsx
+++ learn-context-api/src/components/profile.jsx
@@ -2,12 +2,23 @@ import React, {useContext} from 'react'
 import UserContext from '../context/UserContext'
 
 function Profile() {
-    const {user} = useContext(UserContext)
-    
-    if (!user) return <div>please login</div>
+  const { user } = useContext(UserContext);
 
-    return <div>Welcome {user.username}</div>
+  if (!user) {
+    return (
+      <p className="mt-4 text-center text-gray-600">
+        Please login
+      </p>
+    );
+  }
+
+  return (
+    <p className="mt-4 text-center text-green-600 font-semibold">
+      Welcome {user.username}
+    </p>
+  );
 }
+
 
 export default Profile
 

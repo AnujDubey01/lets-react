997c2f949c3bd66b284aab71adb510811702a6b8
AnujDubey01 <anujdubey.ok@gmail.com>
2026-01-17 00:45:26 +0530

    unknown change

--- MegaBlog/src/main.jsx
+++ MegaBlog/src/main.jsx
@@ -1,18 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
 import { Provider } from 'react-redux'
 import store from './store/store.js'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Home from './pages/Home.jsx'
-import { AuthLayout, Login, Signup } from './components'
-import AddPost from './pages/AddPost.jsx'
-import AllPosts from './pages/AllPosts.jsx'
-import EditPost from './pages/EditPost.jsx'
-import Post from './pages/Post.jsx'
-import LoginPage from './pages/Login.jsx'
-import SignupPage from './pages/Signup.jsx'
+import { AuthLayout, Login } from './components/index.js'
+
+
+import AddPost from "./pages/AddPost";
+import Signup from './pages/Signup'
+import EditPost from "./pages/EditPost";
+
+import Post from "./pages/Post";
+
+import AllPosts from "./pages/AllPosts";
 
 const router = createBrowserRouter([
   {
@@ -27,7 +30,7 @@ const router = createBrowserRouter([
             path: "/login",
             element: (
                 <AuthLayout authentication={false}>
-                    <LoginPage />
+                    <Login />
                 </AuthLayout>
             ),
         },
@@ -35,7 +38,7 @@ const router = createBrowserRouter([
             path: "/signup",
             element: (
                 <AuthLayout authentication={false}>
-                    <SignupPage />
+                    <Signup />
                 </AuthLayout>
             ),
         },
@@ -43,6 +46,7 @@ const router = createBrowserRouter([
             path: "/all-posts",
             element: (
                 <AuthLayout authentication>
+                    {" "}
                     <AllPosts />
                 </AuthLayout>
             ),
@@ -51,6 +55,7 @@ const router = createBrowserRouter([
             path: "/add-post",
             element: (
                 <AuthLayout authentication>
+                    {" "}
                     <AddPost />
                 </AuthLayout>
             ),
@@ -59,6 +64,7 @@ const router = createBrowserRouter([
             path: "/edit-post/:slug",
             element: (
                 <AuthLayout authentication>
+                    {" "}
                     <EditPost />
                 </AuthLayout>
             ),
@@ -68,13 +74,13 @@ const router = createBrowserRouter([
             element: <Post />,
         },
     ],
-  },
+},
 ])
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+    <RouterProvider router={router}/>
     </Provider>
-  </StrictMode>,
+  </React.StrictMode>,
 )
